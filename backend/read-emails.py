11c55a164c1a4168d37b32bc3b57509d22aad1f0
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2023-08-16 14:52:42 +0100

    Implement basic code to loop through all emails in the past day

--- backend/read-emails.py
+++ backend/read-emails.py
@@ -1,4 +1,19 @@
 from simplegmail import Gmail
+from simplegmail.query import construct_query
 
-print("Hello")
+
 gmail = Gmail()
+
+query_params = {
+    "newer_than": (1, "day"),
+}
+
+messages = gmail.get_messages(query=construct_query(query_params))
+
+for message in messages:
+    print("To: " + message.recipient)
+    print("From: " + message.sender)
+    print("Subject: " + message.subject)
+    print("Date: " + message.date)
+    print("Preview: " + message.snippet)
+    print("Message Body: " + message.plain)
