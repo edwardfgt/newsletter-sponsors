3f4eebf2283682a7c8b96ff24aa1691fe2dba01c
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2023-08-16 22:40:45 +0100

    Test connection to Mongo Atlas

--- backend/read-emails.py
+++ backend/read-emails.py
@@ -1,6 +1,18 @@
 from simplegmail import Gmail
 from simplegmail.query import construct_query
+from pymongo.mongo_client import MongoClient
+import config
 
+
+uri = f"mongodb+srv://Edward:{config.mongoPw}@emails.443qzuu.mongodb.net/?retryWrites=true&w=majority"
+
+client = MongoClient(uri)
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
 
 gmail = Gmail()
 
@@ -10,10 +22,17 @@ query_params = {
 
 messages = gmail.get_messages(query=construct_query(query_params))
 
-for message in messages:
-    print("To: " + message.recipient)
-    print("From: " + message.sender)
-    print("Subject: " + message.subject)
-    print("Date: " + message.date)
-    print("Preview: " + message.snippet)
-    print("Message Body: " + message.plain)
+# for message in messages:
+#     print("From: " + message.sender)
+#     print("Subject: " + message.subject)
+#     print("Date: " + message.date)
+#     print("Preview: " + message.snippet)
+#     print("Message Body: " + message.plain)
+
+
+# {
+#   "from": "Jane Doe",
+#   "subject": "jane@abc.com",
+#   "date": 26,
+#   "body": "hello",
+# }
