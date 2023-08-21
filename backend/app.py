9cf12b17d67122103d2f11e4b6d14dc7dfaf5a4b
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2023-08-21 23:39:39 +0100

    Test OpenAI API to replace scraping function

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,9 @@
 from pymongo.mongo_client import MongoClient
 import config
-import newsletters
-import re
-from sponsor_scraper import find_sponsor
+import os
+import openai
+
+openai.api_key = config.gpt
 
 uri = f"mongodb+srv://Edward:{config.mongoPw}@emails.443qzuu.mongodb.net/?retryWrites=true&w=majority"
 
@@ -13,14 +14,27 @@ collection = db.Emails
 
 all_records = collection.find()
 
+num_records_to_process = 10  
+processed_records = 0
+
 for record in all_records:
-    body = record.get('body', '')  # Replace with the actual field name
+    if processed_records >= num_records_to_process:
+        break
+    
+    newsletter = record.get('from', '')
+    body = record.get('body', '')
 
-    sponsor = find_sponsor(body)  # Replace with your sponsor extraction logic
-    if sponsor:
-        print(f"Sponsor: {sponsor}")
-    else:
-        print("Sponsor not found.")
+    conversation = [
+        {"role": "user", "content": f"Please read this email sent from {newsletter} and identify the sponsor, only return a single string with the sponsor companies name:\n{body}"}
+    ]
 
-# Close the MongoDB client connection
+    chat_completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=conversation
+    )
+
+    model_response = chat_completion.choices[0].message['content']
+    print(f"{newsletter} sponsor is: {model_response}")
+    processed_records += 1
+
 client.close()
