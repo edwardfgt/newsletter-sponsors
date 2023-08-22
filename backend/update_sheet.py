c61a0b2549524cf9c2eb72b9aa0584510f89b33f
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2023-08-22 15:13:02 +0100

    Create basic function to append Google sheet with mongo db records

--- backend/update_sheet.py
+++ backend/update_sheet.py
@@ -1,7 +1,24 @@
 import gspread
+from pymongo.mongo_client import MongoClient
+import config
 
-sa = gspread.service_account(filename="sheets_secret.json")
-sh = sa.open("Email Sponsors")
-wks = sh.worksheet("Sponsors")
+uri = f"mongodb+srv://Edward:{config.mongoPw}@emails.443qzuu.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(uri)
+db = client.sponsorScraper
+collection = db.Emails 
+emails = collection.find()
 
-print("rows: ", wks.row_count)
+
+def update_sheet(emails):
+
+    sa = gspread.service_account(filename="sheets_secret.json")
+    sh = sa.open("Email Sponsors")
+    wks = sh.worksheet("Sponsors")
+
+    for email in emails:
+        row = [email['date'], email['from'], email['sponsor']]
+        wks.append_row(row)
+
+
+
+update_sheet(emails)
